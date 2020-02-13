875e3a95f3d95f11cb779f8019ec658d87e63d76
Extro <marco.tan.200405@gmail.com>
2020-02-13 15:19:50 -0500

    Separating PROS and OkapiLib, frame of drive code.

---
+++ include/subsystem_headers/drive.hpp
@@ -0,0 +1,11 @@
+//Start of header guard
+#ifndef DRIVE_HPP
+#define DRIVE_HPP
+
+//Declare function prototypes
+extern void drive_op(void);
+extern void drive_aut_simple();
+extern void drive_aut_path();
+
+//End of header guard
+#endif
