9100818d88fd77b2db3ec2872e843bcfdb800018
Extro <marco.tan.200405@gmail.com>
2020-02-13 15:24:30 -0500

    Working on driver code.

---
+++ include/subsystem_headers/drive.hpp
@@ -0,0 +1,12 @@
+//Include necessary headers
+#include "api.h"
+
+//Start of header guard
+#ifndef DRIVE_HPP
+#define DRIVE_HPP
+
+//Define drive functions
+extern void drive_op(void);
+
+//End of header guard
+#endif
